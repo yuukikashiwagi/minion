4fdd252daa0e6d615e04df0b3509d7fd89f4cf79
yuukikashiwagi <yuuki_kashiwagi@demia.co.jp>
2024-09-19 14:55:26 +0900

    js分割

--- js/index.js
+++ js/index.js
@@ -1,21 +1,21 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//     var sensorBtn = document.getElementById("sensor_permission");
-//     sensorBtn.addEventListener("click", () => {
-//         if (window.DeviceOrientationEvent && window.DeviceOrientationEvent.requestPermission) {
-//             DeviceOrientationEvent.requestPermission()
-//                 .then((state) => {
-//                     if (state === 'granted') {
-//                         // window.location.href = "./content/index.html#score";
-//                         alert('Permission granted for DeviceOrientationEvent');
-//                     } else {
+document.addEventListener("DOMContentLoaded", () => {
+    var sensorBtn = document.getElementById("sensor_permission");
+    sensorBtn.addEventListener("click", () => {
+        if (window.DeviceOrientationEvent && window.DeviceOrientationEvent.requestPermission) {
+            DeviceOrientationEvent.requestPermission()
+                .then((state) => {
+                    if (state === 'granted') {
+                        // window.location.href = "./content/index.html#score";
+                        alert('Permission granted for DeviceOrientationEvent');
+                    } else {
 
-//                         alert('Permission not granted for DeviceOrientationEvent');
-//                     }
-//                 })
-//                 .catch((err) => console.error(err));
-//         } else {
-//             alert('まず、そんなものないね');
-//             // window.location.href = "./content/index.html#score";
-//         }
-//     })
-// })
+                        alert('Permission not granted for DeviceOrientationEvent');
+                    }
+                })
+                .catch((err) => console.error(err));
+        } else {
+            alert('まず、そんなものないね');
+            // window.location.href = "./content/index.html#score";
+        }
+    })
+})
