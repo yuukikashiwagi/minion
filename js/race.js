fc6c77d086c07a44296b2945b8ab1de5b8fa2a9a
yuukikashiwagi <yuuki_kashiwagi@demia.co.jp>
2024-09-19 15:37:37 +0900

    console確認

--- js/race.js
+++ js/race.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
 
+console("fghjk")
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera(
     75,
